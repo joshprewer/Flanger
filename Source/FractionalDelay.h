95f437aa4a861e38cc485f9e91c8e8eda39779f6
Josh Prewer <joshprewer@usr.lan>
2016-08-18 11:07:14 +0100

    Initial Commit

---
+++ Source/FractionalDelay.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    FractionalDelay.h
+    Created: 18 Aug 2016 11:06:15am
+    Author:  Josh Prewer
+
+  ==============================================================================
+*/
+
+#ifndef FRACTIONALDELAY_H_INCLUDED
+#define FRACTIONALDELAY_H_INCLUDED
+
+
+
+
+
+#endif  // FRACTIONALDELAY_H_INCLUDED
---
+++ Source/SineGenerator.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    SineGenerator.h
+    Created: 18 Aug 2016 11:06:07am
+    Author:  Josh Prewer
+
+  ==============================================================================
+*/
+
+#ifndef SINEGENERATOR_H_INCLUDED
+#define SINEGENERATOR_H_INCLUDED
+
+
+
+
+
+#endif  // SINEGENERATOR_H_INCLUDED
